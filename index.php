8e21cfbfc2193b051e7c14199470a92e23250592
xxxx <xxxx>
2018-09-14 11:56:17 +0530

    Proj

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+		<nav class="navbar navbar-inverse">
+			
+			<div class="container-fluid">
+				
+				<div class="navbar-header">
+					<!-- Logo -->
+					<a href="#" class="navbar-brand">AUTOMOBILES</a>
+				</div>
+
+				<!-- Menu Items-->
+				<div>
+					<ul class="nav navbar-nav ml-auto">
+						<li class="nav-item"><a href="#"  class="nav-link">Home</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">About</a></li>
+						<li class="nav-item"><a href="#"
+						class="nav-link">Content</a></li>
+						
+					</ul>
+				</div>
+
+			</div>
+
+		</nav>
+
+</body>
+</html>
