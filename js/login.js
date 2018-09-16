5bb44128f67b29a1942e8935f87d4333e8c338fa
Ryan Noronha <ryannor56a@gmail.com>
2018-09-16 13:07:10 +0530

    Added login functionality. Testing required (Read code for further TODOs)

---
+++ js/login.js
@@ -0,0 +1,57 @@
+var loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', function(e) {
+    // Flag variables.
+    var employeeID = false;
+    var employeePassword = false;
+
+    // Check if ID is present.
+    if (!$('#employeeID').val()) {
+        // Add a line when ID field is empty.
+        if($('#employeeID').parent().next('.validation').length == 0) {
+            $('#employeeID').parent().after('<div class="validation" style="color: red; margin-bottom: 20px;">Please enter your ID</div>');
+        }
+        e.preventDefault();  // Prevent POST to server.
+        // Focus on ID field if ID is not present.
+        $('#employeeID').focus();
+    }
+    else {
+        // If ID is added, then remove the warning line.
+        $('#employeeID').parent().next('.validation').remove();
+        employeeID = true;
+    }
+
+    // Perform the same checks for the password field.
+    if (!$('#employeePassword').val()) {
+        if($('#employeePassword').parent().next('.validation').length == 0) {
+            $('#employeePassword').parent().after('<div class="validation" style="color: red; margin-bottom: 20px;">Please enter your password</div>');
+        }
+        e.preventDefault();
+        $('#employeePassword').focus();
+    }
+    else {
+        $('#employeePassword').parent().next('.validation').remove();
+        employeePassword = true
+    }
+
+    // Call server side code to log the employee in.
+    // 'log-employee.php' yet to be created.
+    if (employeeID && employeePassword) {
+        $.ajax({
+            url: './log-employee.php',
+            type: 'POST',
+            dataType: 'json',
+            data: {functionname: 'login', arguments: [$('#employeeID').val(), $('#employeePassword').val()]},
+            async: false,
+            success: function(obj, textstatus) {
+                if (!('error' in obj)) {
+                    alert('Sign in successful!');
+                }
+                else {
+                    console.log(obj.error);
+                }
+            }
+        });
+        
+    }
+});
---
+++ navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark sticky-top" style="height: 80px;">
+    <a href="#" class="navbar-brand">AUTOMOBILES</a>
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarMenu">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="ind2.php" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Cars</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Bikes</a>
+            </li>
+            <li class="nav-item">
+                <a href="login.php" class="nav-link">Login</a>
+            </li>
+        </ul>
+    </div>
+</nav>
