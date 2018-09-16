97dd7b79fcadb57b89e1e849d1f7be095e805fc0
Ryan Noronha <ryannor56a@gmail.com>
2018-09-16 18:21:44 +0530

    Added PHP file to handle the request for loging in. Currently only dummy values will be returned. Need further assistance to solve issue.

--- js/login.js
+++ js/login.js
@@ -31,27 +31,51 @@ loginForm.addEventListener('submit', function(e) {
     }
     else {
         $('#employeePassword').parent().next('.validation').remove();
-        employeePassword = true
+        employeePassword = true;
     }
 
     // Call server side code to log the employee in.
     // 'log-employee.php' yet to be created.
     if (employeeID && employeePassword) {
+        e.preventDefault();
         $.ajax({
-            url: './log-employee.php',
+            url: '/log-employee.php',
             type: 'POST',
             dataType: 'json',
             data: {functionname: 'login', arguments: [$('#employeeID').val(), $('#employeePassword').val()]},
-            async: false,
-            success: function(obj, textstatus) {
-                if (!('error' in obj)) {
-                    alert('Sign in successful!');
-                }
-                else {
-                    console.log(obj.error);
-                }
+        })
+        .done(function(obj) {
+            res = JSON.parse(obj);
+
+            if (res['response']) {
+                alert('Sign in successful!');
+                console.log('Ya YEET!');
             }
+            else {
+                alert('OOPS! Something went wrong!');
+                console.log(obj.error);
+            }
+        })
+        .fail(function() {
+            alert('error!');
         });
-        
+
+        // $.ajax({
+        //     url: 'log-employee.php',
+        //     type: 'POST',
+        //     dataType: 'json',
+        //     data: {functionname: 'login', arguments: [$('#employeeID').val(), $('#employeePassword').val()]},
+        //     async: false,
+        //     success: function(obj, textstatus) {
+        //         if (!('error' in obj)) {
+        //             alert('Sign in successful!');
+        //             console.log('Ya YEET!')
+        //         }
+        //         else {
+        //             alert('OOPS! Something went wrong!')
+        //             console.log(obj.error);
+        //         }
+        //     }
+        // });  
     }
 });
--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-        <title>Hello, world!</title>
+        <title>Employee login - Automobile Dealership</title>
     </head>
     <body>
         <?php include 'navbar.php'; ?>
@@ -15,7 +15,7 @@
             <form method="POST" id="loginForm">
                 <div class="form-group">
                     <p><label for="employeeID">Employee ID</label></p>
-                    <p><input type="email" class="form-control" id="employeeID" aria-describedby="emailHelp" placeholder="Employee ID" autofocus></p>
+                    <p><input type="id" class="form-control" id="employeeID" placeholder="Employee ID" autofocus></p>
                 </div>
 
                 <div class="form-group">
@@ -27,7 +27,7 @@
             </form>
         </div>
         
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="js/jquery-3.3.1.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <script src="js/login.js"></script>
